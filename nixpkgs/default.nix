00e6aed52d3f790da707a1cc7722c0d9268d15e3
Will Fancher <elvishjerricco@gmail.com>
2017-06-15 14:03:15 -0400

    Use reflex-platform style hack-on-able nixpkgs directory

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,6 +1,1 @@
-import ((import <nixpkgs> {}).fetchFromGitHub {
-  owner = "WebGHC";
-  repo = "nixpkgs";
-  rev = "88364c4beaa0bf61d361d520a66a6b66c3de768b";
-  sha256 = "0vym6j809f3sw11bz6plsfiwmd65z50k9fsvqw01pj4kfvjx78sr";
-})
+import ((import <nixpkgs> {}).fetchFromGitHub (builtins.fromJSON (builtins.readFile ./github.json)))
