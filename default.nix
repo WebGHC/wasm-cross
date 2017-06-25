d3c9d1564d37686f9bf4bb99676c2e313e9159fd
Will Fancher <elvishjerricco@gmail.com>
2017-06-25 19:41:14 -0400

    Set default llvmPackages to HEAD

--- default.nix
+++ default.nix
@@ -4,6 +4,8 @@
       cmake-hello-world = self.callPackage ./cmake-hello-world {};
 
       llvmPackages_HEAD = self.callPackage ./llvm-head { buildTools = self.buildPackages.llvmPackages_HEAD; };
+
+      llvmPackages = self.llvmPackages_HEAD;
     })];
   };
   nixpkgsCrossArgs = wasm-cross-self.nixpkgsArgs // {};
