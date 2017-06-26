c9e9bf7e7197a3e7ed62f03527bccb1a1734d8fd
Will Fancher <elvishjerricco@gmail.com>
2017-06-25 20:03:48 -0400

    Fix libunwind

--- llvm-head/libunwind.nix
+++ llvm-head/libunwind.nix
@@ -5,6 +5,7 @@
 , hostPlatform
 , buildPlatform
 , lib
+, libcxx-headers
 }:
 stdenv.mkDerivation {
   name = "libunwind";
@@ -14,11 +15,13 @@ stdenv.mkDerivation {
     sha256 = "0sbgy1qmggyijb4axksm6chiq3ymyaay5ndjidbf6qq9s0ad1ybd";
   };
   nativeBuildInputs = [ cmake ];
+  buildInputs = [ libcxx-headers ];
   cmakeFlags = [
     "-DLLVM_CONFIG_PATH=${llvm}/bin/llvm-config"
     "-DLIBUNWIND_TARGET_TRIPLE=${hostPlatform.config}"
-  ] ++ lib.optionals (hostPlatform != buildPlatform) [
+    "-DCMAKE_CXX_FLAGS=-I${libcxx-headers}/include"
     "-DLLVM_NO_OLD_LIBSTDCXX=TRUE"
+  ] ++ lib.optionals (hostPlatform != buildPlatform) [
     "-DUNIX=TRUE" # TODO: Figure out what this is about
   ];
 
