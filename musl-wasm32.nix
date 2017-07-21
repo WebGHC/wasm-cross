539389924d295436b7f53c4bf1e38e6bcd33ea7f
Michael Vogelsang <mvogelsang@rocketmail.com>
2017-07-20 22:10:05 -0400

    bump wasm-syslib-builder

--- musl-wasm32.nix
+++ musl-wasm32.nix
@@ -4,26 +4,9 @@ stdenv.mkDerivation {
   name = "musl";
   src = fetchgit {
     url = "https://github.com/WebGHC/wasm-syslib-builder";
-    rev = "cc5b9cdb7479db3ad781a01d4f21d4dfa46e6adc";
-    sha256 = "1445a537y0jdf61cc68nrw65iljqv20p3ddv8qb8zqi8q49n0gfl";
+    rev = "ae1446d70619e6b5f99fa49fe34cc23264d46d7e";
+    sha256 = "1zp43pf5yg2vrc1c3w29vfv2xrjk788h58lfqxf0ww78ifpr6kal";
   };
-  phases = ["unpackPhase" "buildPhase" "installPhase"];
-  nativeBuildInputs = [buildPackages.python];
+  
   hardeningDisable = ["pic"];
-
-  buildPhase = ''
-    python libbuild.py
-  '';
-
-  installPhase = ''
-    mkdir -p $out/lib
-    mkdir -p $out/include
-
-    cp `pwd`/lib/libc.a $out/lib
-    cp -r `pwd`/emscripten/system/include/libc/* $out/include
-    rm $out/include/bits # This comes from arch/emscripten
-    cp -r `pwd`/emscripten/system/include/emscripten.h $out/include
-    cp -r `pwd`/emscripten/system/include/emscripten/* $out/include
-    cp -r `pwd`/emscripten/system/lib/libc/musl/arch/emscripten/* $out/include
-  '';
 }
