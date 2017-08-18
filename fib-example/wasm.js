449a060a2eb4f08663104195eef01778873d898e
Will Fancher <elvishjerricco@gmail.com>
2017-08-18 00:51:21 -0400

    Simplify wasm.js

--- fib-example/wasm.js
+++ fib-example/wasm.js
@@ -1,3 +1,9 @@
+var importObject = {
+    "env": {
+	// JS functions required by the wasm module go here
+    }
+};
+
 function fetchAndInstantiate(url, importObject) {
   return fetch(url).then(response =>
     response.arrayBuffer()
@@ -8,33 +14,8 @@ function fetchAndInstantiate(url, importObject) {
   );
 }
 
-
-
-var importObject = {
-    "env": {
-        "__eqtf2": () => {throw "NYI"},
-        "__extenddftf2": () => {throw "NYI"},
-        "__fixtfsi": () => {throw "NYI"},
-        "__fixunstfsi": () => {throw "NYI"},
-        "__floatsitf": () => {throw "NYI"},
-        "__floatunsitf": () => {throw "NYI"},
-        "getenv": () => {throw "NYI"},
-        "__lock": () => {throw "NYI"},
-        "__map_file": () => {throw "NYI"},
-        "__netf2": () => {throw "NYI"},
-        "sbrk": () => {throw "NYI"},
-        "__stack_chk_fail": () => {throw "NYI"},
-        "__stack_chk_guard": () => {throw "NYI"},
-        "__syscall140": () => {throw "NYI"},
-        "__syscall146": () => {throw "NYI"},
-        "__syscall6": () => {throw "NYI"},
-        "__syscall91": () => {throw "NYI"},
-        "__unlock": () => {throw "NYI"},
-        "__unordtf2": () => {throw "NYI"},
-    }
-};
-
 fetchAndInstantiate("fib", importObject).then(function(instance) {
     console.log(instance.exports.main);
     console.log(instance.exports.main());
+    console.log(instance.exports.fib(7));
 });
