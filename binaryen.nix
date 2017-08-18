c013bcdcd90a9c9bfd3ca7252179972b32809dfd
Will Fancher <elvishjerricco@gmail.com>
2017-08-18 17:01:10 -0400

    Add a gross patch to binaryen to support debugging
    TODO: Find the correct place to file this issue

--- binaryen.nix
+++ binaryen.nix
@@ -9,4 +9,14 @@ stdenv.mkDerivation {
     sha256 = "1hfkz6q4k99qz0hhbygdfwqnsf0rdjghbj1bqy25zamsbmzq67ch";
   };
   nativeBuildInputs = [ cmake ];
+
+  # Binaryen (correctly) fails on symbols with the same
+  # name. Something is outputting multiple `dummy` symbols when you
+  # call a big enough libc function (like `printf`, as long as your
+  # call can't be optimized to `puts`), which works fine as far as the
+  # browser is concerned, since it only cares about the numeric
+  # identifier. But to inspect such wasm, we have to patch binaryen to
+  # produce (invalid) disassembly in this case. Considering binaryen
+  # has no part in our toolchain currently, this is acceptable.
+  patches = [./binaryen.patch];
 }
