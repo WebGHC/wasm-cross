91d281f1d01d23cc5f15c4b68b4f889908d8f18f
Will Fancher <elvishjerricco@gmail.com>
2017-08-31 04:07:04 -0400

    Added nixpkgsArm.haskellPackages.hello to ci.nix

--- ci.nix
+++ ci.nix
@@ -5,6 +5,8 @@ with import ./.;
   nixpkgs.llvmPackages_HEAD
   nixpkgs.binaryen
   nixpkgs.haskell.compiler.ghcHEAD
+
+  nixpkgsArm.haskellPackages.hello
 ] ++ builtins.concatLists (builtins.map (pkgs: [
   pkgs.stdenv.cc
   pkgs.fib-example
