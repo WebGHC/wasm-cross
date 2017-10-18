f658ef54ddba68d2379eb1e65ab25ba920d6e101
Will Fancher <elvishjerricco@gmail.com>
2017-10-17 21:01:34 -0400

    extra derp

--- release.nix
+++ release.nix
@@ -7,10 +7,12 @@ let
     inherit (pkgs) fib-example;
   };
 in {
+  inherit (nixpkgs.llvmPackages_HEAD) llvm clang compiler-rt libcxx
+    libcxx-headers libcxxabi libunwind lld lldb llvm-binutils;
   inherit (nixpkgs) binaryen;
   inherit (nixpkgs.haskell.compiler) ghcHEAD;
   inherit (nixpkgsArm.haskellPackages) hello;
 
-  wasm = fromPkgs nixpkgsWasm;
-  arm = fromPkgs nixpkgsArm;
+  wasm = nixpkgs.recurseIntoAttrs (fromPkgs nixpkgsWasm);
+  arm = nixpkgs.recurseIntoAttrs (fromPkgs nixpkgsArm);
 }
