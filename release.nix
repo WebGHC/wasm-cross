419f84f22bb79512a6b89b36f37cd8e7b2ab8087
Will Fancher <elvishjerricco@gmail.com>
2017-10-20 19:59:41 -0400

    Slightly more detailed release.nix

--- release.nix
+++ release.nix
@@ -7,12 +7,13 @@ let
     inherit (pkgs) fib-example;
   };
 in {
-  inherit (nixpkgs.llvmPackages_HEAD) llvm clang compiler-rt libcxx
-    libcxx-headers libcxxabi libunwind lld lldb llvm-binutils;
+  inherit (nixpkgs.llvmPackages_HEAD) llvm clang clang-unwrapped compiler-rt
+    libcxx libcxx-headers libcxxabi libunwind lld lldb llvm-binutils;
   inherit (nixpkgs) binaryen;
   inherit (nixpkgs.haskell.compiler) ghcHEAD;
-  inherit (nixpkgsArm.haskellPackages) hello;
 
   wasm = nixpkgs.recurseIntoAttrs (fromPkgs nixpkgsWasm);
-  arm = nixpkgs.recurseIntoAttrs (fromPkgs nixpkgsArm);
+  arm = nixpkgs.recurseIntoAttrs (fromPkgs nixpkgsArm) // {
+    inherit (nixpkgsArm.haskellPackages) hello ghc;
+  };
 }
