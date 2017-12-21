358496ba6405cccae9196b3230b7160fb1e42539
mjvoge02 <mvogelsang@rocketmail.com>
2017-12-20 23:39:39 -0500

    add default.nix

--- default.nix
+++ default.nix
@@ -6,6 +6,8 @@
       fib-example = self.callPackage ./fib-example {};
 
       hello-example = self.callPackage ./hello-example {};
+      free-example = self.callPackage ./free-example {};
+
 
       llvmPackages_HEAD = self.callPackage ./llvm-head {
         buildTools = self.buildPackages.llvmPackages_HEAD;
@@ -41,7 +43,7 @@
       disableDynamicLinker = true;
       thread-model = "single";
       # target-cpu = "bleeding-edge";
-      # entry = "main";
+      entry = "main";
     };
   });
   nixpkgsArm = import ./nixpkgs (project.nixpkgsCrossArgs // {
