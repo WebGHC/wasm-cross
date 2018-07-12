881522128be43eafd100044926f2fcd63d724b26
Will Fancher <elvishjerricco@gmail.com>
2018-07-12 05:18:27 -0400

    Bump webabi

--- webabi-nix/node-packages.nix
+++ webabi-nix/node-packages.nix
@@ -4,13 +4,13 @@
 
 let
   sources = {
-    "async-2.6.0" = {
+    "async-2.6.1" = {
       name = "async";
       packageName = "async";
-      version = "2.6.0";
+      version = "2.6.1";
       src = fetchurl {
-        url = "https://registry.npmjs.org/async/-/async-2.6.0.tgz";
-        sha512 = "0zp4b5788400npi1ixjry5x3a4m21c8pnknk8v731rgnwnjbp5ijmfcf5ppmn1ap4a04md1s9dr8n9ygdvrmiai590v0k6dby1wc1y4";
+        url = "https://registry.npmjs.org/async/-/async-2.6.1.tgz";
+        sha512 = "2aqgkis9ac37q6jv6zspfpj3rikh2vr9fdch7wajrvqihq5sxyd1gh5zv65hy0y3r22l720qkidwh6img8dngqcjj0dwrl0dwpj5lbw";
       };
     };
     "browserfs-1.4.3" = {
@@ -22,13 +22,13 @@ let
         sha512 = "39jq7fphcz7kwfcwqb7iiy3b47gfihlyhy5mr9rjdvj6d32kxzvlbzywp5wdcqpwjsqn16wm275qphkxg3vp26chmn35kbbal50fgxp";
       };
     };
-    "lodash-4.17.5" = {
+    "lodash-4.17.10" = {
       name = "lodash";
       packageName = "lodash";
-      version = "4.17.5";
+      version = "4.17.10";
       src = fetchurl {
-        url = "https://registry.npmjs.org/lodash/-/lodash-4.17.5.tgz";
-        sha512 = "11hikgyas884mz8a58vyixaahxbpdwljdw4cb6qp15xa3sfqikp2mm6wgv41jsl34nzsv1hkx9kw3nwczvas5p73whirmaz4sxggwmj";
+        url = "https://registry.npmjs.org/lodash/-/lodash-4.17.10.tgz";
+        sha512 = "1ba5b80jjzwrh9fbdk5ywv8sic0dynij21wgrfxsfjzwvwd7x1n6azdhdc0vjdxqmcpm0mhshd1k7n2ascxpz00z3p8a3k97mwg1s2i";
       };
     };
     "pako-1.0.6" = {
@@ -47,13 +47,13 @@ let
     version = "0.0.1";
     src = fetchgit {
       url = "https://github.com/WebGHC/webabi.git";
-      rev = "7e70d2ea79f3d90ab9ba5638cb46d97a4e159150";
-      sha256 = "0g6a4sg02r1skndjxa6n2af2mw375p3ca2p785hipkjl8z57vdjn";
+      rev = "450e6aacbde9c3413e9eff658674aca483f992e3";
+      sha256 = "1bn2qrxxzsx7a5f3ribmlzq2z12pkf59lmy728x8zy9f62zhad17";
     };
     dependencies = [
-      sources."async-2.6.0"
+      sources."async-2.6.1"
       sources."browserfs-1.4.3"
-      sources."lodash-4.17.5"
+      sources."lodash-4.17.10"
       sources."pako-1.0.6"
     ];
     buildInputs = globalBuildInputs;
