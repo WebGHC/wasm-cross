b93b13e3ddc0bbe6b11e672e10cdac6cef35c762
Will Fancher <elvishjerricco@gmail.com>
2019-01-08 21:21:13 -0500

    Replace nixpkgs submodule with fetchTarball
    We don't do much on nixpkgs itself anymore. This way, cloning
    wasm-cross is easier

---
+++ nixpkgs/default.nix
@@ -0,0 +1,4 @@
+import (builtins.fetchTarball {
+  url = https://github.com/WebGHC/nixpkgs/archive/ac4f980f897391e94df587cb74766c4407562a83.tar.gz;
+  sha256 = "01nignzgyd95rs7ia6gc3rgxn9bk467ydf38b3fg9m1qxic94qcx";
+})
