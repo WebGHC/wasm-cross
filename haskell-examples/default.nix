09d6fa9780a49e600d6eb01ffb3819f656c5626c
Divam <dfordivam@gmail.com>
2019-12-13 06:22:17 +0900

    Use pkgs.fetchgit as the CI fails with builtins.fetchGit

--- haskell-examples/default.nix
+++ haskell-examples/default.nix
@@ -1,15 +1,15 @@
 pkgs: pkgsSuper:
 
 let
-  jsaddle-src = builtins.fetchGit {
+  jsaddle-src = pkgs.fetchgit {
     url = https://github.com/WebGHC/jsaddle;
     rev = "d55c7f6e04de35221e913fe2841551f6ff29b229";
-    ref = "ghc881-fixes";
+    sha256 = "061b93705ybvph8nh2j3bplw9qgshnb5l4njixdb4df3aa9wvvip";
   };
-  reflex-examples-src = builtins.fetchGit {
+  reflex-examples-src = pkgs.fetchgit {
     url = https://github.com/dfordivam/reflex-examples;
     rev = "5cb9bed97f9441bd925117b9d743574cab66f017";
-    ref = "patch-1";
+    sha256 = "1s9kksj61v8zfv7vkg420mi8kqg2q68w6qg62930amfbf9cjbb72";
   };
   miso-src = pkgs.fetchgit {
     url = https://github.com/WebGHC/miso;
@@ -17,24 +17,26 @@ let
     sha256 = "0gqf9hzvmk9lzmfcpf9bzb637c1767p0z9p86k2zn8a1p0gs8a9j";
     fetchSubmodules = true;
   };
-  dependent-sum-src = builtins.fetchGit {
+  dependent-sum-src = pkgs.fetchgit {
     url = https://github.com/WebGHC/dependent-sum;
     rev = "5158a7dc5e714ca82e94c76ceec838ad85b0efab";
-    ref = "ghc-881-th-fixes";
+    sha256 = "0k9z63snfdz5rl6lndy2nclk4wpqv60mkbjs8l4jy42ammk8554r";
   };
   haskellLib = pkgs.haskell.lib;
   inherit (pkgs) lib;
   extensions = isWasm: lib.composeExtensions
     (haskellLib.packageSourceOverrides {
       jsaddle = jsaddle-src + /jsaddle;
-      jsaddle-dom = builtins.fetchGit {
+      jsaddle-dom = pkgs.fetchgit {
         url = https://github.com/dfordivam/jsaddle-dom;
         rev = "1260311c1c600a95660cdec4cc88a097099ad876";
+        sha256 = "0jcz8qmvbybd55g845dydkm7yxw3q2pq507c0ifkiamj630jlxnc";
       };
       jsaddle-warp = jsaddle-src + /jsaddle-warp;
-      jsaddle-wasm = builtins.fetchGit {
+      jsaddle-wasm = pkgs.fetchgit {
         url = https://github.com/WebGHC/jsaddle-wasm;
         rev = "4079c78aec6dc7d96ff21580971406fe914ad038";
+        sha256 = "0sswsq5aig1d0z715zc2c3kshsjbp59fsls816rcfni0wdfx01xz";
       };
 
       Keyboard = reflex-examples-src + /Keyboard;
@@ -47,43 +49,45 @@ let
 
       dependent-sum = dependent-sum-src + /dependent-sum;
       dependent-sum-template = dependent-sum-src + /dependent-sum-template;
-      dependent-map = builtins.fetchGit {
+      dependent-map = pkgs.fetchgit {
         url = https://github.com/WebGHC/dependent-map;
         rev = "c28ef5350b3c03c4ff92e669703e4eb67e61ffa5";
+        sha256 = "1a1ha24bsm15pywd51d50gax6ssydhvjr1fcz53vb7i2xpr7iv3f";
       };
       monoidal-containers = "0.6";
       lens = "4.18.1";
       witherable = "0.3.4";
-      prim-uniq = builtins.fetchGit {
+      prim-uniq = pkgs.fetchgit {
         url = https://github.com/WebGHC/prim-uniq;
         rev = "34570a948f7d84a1821ed6d8305ed094c4f6eb15";
+        sha256 = "15xm0ky6dgndn714m99vgxyd4cr782gn0rf8zyf7v8mnj7mhcrc0";
       };
     })
     (self: super:
       let
-        reflex-dom-pkg = import (builtins.fetchGit {
+        reflex-dom-pkg = import (pkgs.fetchgit {
           url = https://github.com/dfordivam/reflex-dom;
           rev = "6bdc206bd83d2db62eb7d98372c9bf4fd355e7da";
-          ref = "wasm-ghc-881";
+          sha256 = "0wa8pzmmyqk31d0rvhwg2hzmrj3rji655lag253l8xdhlc6561sw";
         }) self;
       in {
       mkDerivation = args: super.mkDerivation (args // { doCheck = false; });
       ref-tf = haskellLib.doJailbreak super.ref-tf;
 
-      reflex = self.callPackage (builtins.fetchGit {
+      reflex = self.callPackage (pkgs.fetchgit {
         url = https://github.com/dfordivam/reflex;
         rev = "369b9db9fc50c612123b433848b8423711847462";
-        ref = "ghc-881-fixes";
+        sha256 = "0jbizndl408n5df65fx5haxc60y2vph4wpiznkqr0a92f6ijf94r";
       }) { useTemplateHaskell = false; };
       reflex-dom-core = haskellLib.dontCheck
         (haskellLib.appendConfigureFlag
           (reflex-dom-pkg.reflex-dom-core)
           "-f-use-template-haskell")  ;
       reflex-dom = haskellLib.disableCabalFlag reflex-dom-pkg.reflex-dom "webkit2gtk";
-      reflex-todomvc = self.callPackage (builtins.fetchGit {
+      reflex-todomvc = self.callPackage (pkgs.fetchgit {
         url = https://github.com/reflex-frp/reflex-todomvc;
         rev = "91227b8baa90a6d1e51c803eff7f966dbafe875a";
-        ref = "wasm-8.6.3";
+        sha256 = "0sbpkzxv96z2a11k3nzjw6ik4998si64vspkhii7i00rv0yxc33k";
       }) {};
 
       miso = with haskellLib;
